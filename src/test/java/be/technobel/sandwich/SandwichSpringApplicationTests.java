f96d6c68ac025469a939b0899a300ef0a35eff1f
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2023-02-07 10:32:48 +0100

    Initial commit

---
+++ src/test/java/be/technobel/sandwich/SandwichSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technobel.sandwich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandwichSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
